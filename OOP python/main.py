25caaef68b6167b336620c5e3cf4833872650079
m k <ikawamehmet@gmail.com>
2023-07-02 16:18:38 +0300

    json file hash md5 and class init del load save

--- OOP python/main.py
+++ OOP python/main.py
@@ -1,39 +1,59 @@
 import json
-
-data = {} 
+import hashlib
 
 class User:
     def __init__(self, name, password=None, phone=None):
+        data = self.load()
         self.user_name = name
         self.password = ""
-        self.phone = phone
+        self.phone = ""
+        self.load()
+        
+        if name in data:
+            self.password = data[name]["password"]
+            self.phone = data[name]["phone"]
+
         if password is not None:
-            self.password = password
+            self.password = hashlib.md5(password.encode()).hexdigest()
         print("User created", self.user_name)
+
+        if phone is not None:
+            self.phone = phone
 
     def __del__(self):
         print("User deleted")
     
     def verify(self, password):
-        if self.password == password:
+        if self.password == hashlib.md5(password.encode()).hexdigest():
             print("User verified")
             return True
         else:
             print("Wrong password")
             return False
     def save(self):
-        print("User saved")
+        data = self.load()
         data[self.user_name] = {"password": self.password,
                                 "phone": self.phone}
         with open("data.json", "w") as f:
             f.write(json.dumps(data, indent=2))            
 
-def main():
-    user = User("mehmet",1234)
-    user.save()
-    print(data)
-    user2 = User("emir",5678)
-    user2.save()
-    print(data)
+    def load(self):
+        data = {}
+        with open("data.json", "a+") as f:
+            try:
+                f.seek(0)
+                data = json.load(f)
+            except:
+                data = {}
+        return data
 
+
+def main():
+    mehmet = User("mehmet","1234")
+    mehmet.save()
+    burak = User("burak","1256", "12345678")
+    burak.save()
+    print(mehmet.verify("1234"))
+    emir = User("emir","5678")
+    emir.save()
 main()
