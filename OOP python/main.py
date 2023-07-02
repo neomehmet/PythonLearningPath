382063ee3a179596f18650f238bbb4f608825827
m k <ikawamehmet@gmail.com>
2023-07-02 11:37:10 +0300

    intance attribute and adding class atribute

--- OOP python/main.py
+++ OOP python/main.py
@@ -1,2 +1,11 @@
-#class
-print("hi")
+class Vehicle:
+    pass
+
+xinstance = Vehicle()
+
+Vehicle.classAttribute = "class Attribute added"
+xinstance.instanceAttribute = "instance Attribute added"
+ # class no instance attribute but obj has 
+
+print( Vehicle.classAttribute  )
+print( xinstance.instanceAttribute )
