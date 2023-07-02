9f26138949f30a80b06eace4d5c00b7122df3cb4
m k <ikawamehmet@gmail.com>
2023-07-02 15:55:02 +0300

    init and del, json data file

--- OOP python/main.py
+++ OOP python/main.py
@@ -1,22 +1,39 @@
-area = 12
-panels = list()
-i = area 
-x = 0
-while area > 0 :
-    if i*i == area :
-        panels.append(i*i)
-        area = 0 
-        print( " i = ",i," area = ",area )
-        x = input("Press Enter to continue...")
-        break
-    elif i*i < area  :
-        panels.append(i*i)
-        area = area - i*i
-        print( " i = ",i," area = ",area )
-        x = input("Press Enter to continue...")  
-    i = i - 1
-    if i < 1:
-        i = 1
+import json
 
-print(panels)
-print(panels)
+data = {} 
+
+class User:
+    def __init__(self, name, password=None, phone=None):
+        self.user_name = name
+        self.password = ""
+        self.phone = phone
+        if password is not None:
+            self.password = password
+        print("User created", self.user_name)
+
+    def __del__(self):
+        print("User deleted")
+    
+    def verify(self, password):
+        if self.password == password:
+            print("User verified")
+            return True
+        else:
+            print("Wrong password")
+            return False
+    def save(self):
+        print("User saved")
+        data[self.user_name] = {"password": self.password,
+                                "phone": self.phone}
+        with open("data.json", "w") as f:
+            f.write(json.dumps(data, indent=2))            
+
+def main():
+    user = User("mehmet",1234)
+    user.save()
+    print(data)
+    user2 = User("emir",5678)
+    user2.save()
+    print(data)
+
+main()
